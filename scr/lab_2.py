18bfd1038754761bf55a04572f53b9f8d8c4ab6e
Zaberura <zaberura@gmail.com>
2023-09-14 15:24:19 +0300

    Second lab 3

--- scr/lab_2.py
+++ scr/lab_2.py
@@ -1,6 +1,9 @@
 import math
 from mpmath import csc, sec
 
+# Видаліть усі коментарі, "Unnecessary lines" та підправте код під себе, свій варіант
+
+# Вхідні дані з таблиці, ввести свій варіант
 h = 0.2
 x_min = 1.5
 x_max = 3.5
@@ -8,17 +11,27 @@ x_max = 3.5
 
 def calculate_x (min_value: int, max_value: int, step: int):
     x = min_value
+    result = 0
 
     while x <= max_value:
 
+        # з таблиці
         if x < 2:
-            print(csc(math.pow(math.e, x)))
+            # обчислюємо рівняння 1 з таблиці, виводимо результатsc(math.pow(math.e, x))
+            print(result)
+        # з таблиці
         elif 2 <= x < 3:
-            print(sec(math.log(x)))
+            # обчислюємо рівняння 2 з таблиці, виводимо результат
+            result = sec(math.log(x))
+            print(result)
+        # з таблиці
         elif x >= 3:
-            print(math.sin(math.log(x)))
-
+            # обчислюємо рівняння 3 з таблиці, виводимо результат
+            result = math.sin(math.log(x))
+            print(result)
+        # додаємо до Х - h, крок таблюювання зазначений в таблиці
         x += step
 
 
+# викликаємо зроблену функцію і передаємо вхідні дані
 calculate_x(x_min, x_max, h)
